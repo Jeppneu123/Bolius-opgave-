870988adb11cde2bb51825abb1e6020157831fda
Jeppeneu123 <jeppeneumann@live.dk>
2022-12-07 11:20:39 +0100

    Editing of start-page 3.0

--- main.js
+++ main.js
@@ -20,4 +20,35 @@ fetch('https://api.dataforsyningen.dk/adresser?q=' + address )
             } )
 })
 
+const dropDownMenu = document.querySelector(".dropButton")
 
+
+dropDownMenu.addEventListener("click",toggleDropDowns)
+
+function toggleDropDowns()
+{
+
+    document.getElementById("myDropDowns").classList.toggle("show-drop-downs");
+    let removeClass = document.querySelector("#myDropDowns")
+    removeClass.classList.toggle("dropDowns")
+}
+
+
+/*
+window.onclick = function (event)
+{
+    if (!event.target.matches(".dropButton"))
+    {
+        let dropDowns = document.querySelector(".dropDowns")
+        for (let i = 0; i < dropDowns.length; i++)
+        {
+            let openDropDown = dropDowns[i];
+            if (openDropDown.classList.contains("show"))
+            {
+                openDropDown.classList.remove("show")
+            }
+        }
+    }
+}
+
+ */
